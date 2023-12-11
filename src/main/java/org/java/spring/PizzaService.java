098684cce45b155d2091222f249598fc07bc642c
marcocareddu <saldumaccu@tuta.io>
2023-12-11 12:22:21 +0100

    Added IngredientRepository

--- src/main/java/org/java/spring/PizzaService.java
+++ src/main/java/org/java/spring/PizzaService.java
@@ -2,6 +2,7 @@ package org.java.spring;
 
 import java.util.List;
 
+import org.java.spring.repo.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
--- src/main/java/org/java/spring/repo/PizzaRepository.java
+++ src/main/java/org/java/spring/repo/PizzaRepository.java
@@ -1,6 +1,8 @@
-package org.java.spring;
+package org.java.spring.repo;
 
 import java.util.List;
+
+import org.java.spring.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
